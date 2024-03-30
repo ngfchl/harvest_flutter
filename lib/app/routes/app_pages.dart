9070a48c32731542acbedd79672b587f955526ad
ngfchl <ngfchl@126.com>
2024-03-30 20:56:07 +0800

    登录也添加到总路由

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,10 +1,10 @@
 import 'package:get/get.dart';
 
-import '../../middleware/login_middleware.dart';
 import '../home/home_binding.dart';
 import '../home/home_view.dart';
-import '../login/login_binding.dart';
-import '../login/login_view.dart';
+import '../login/binding.dart';
+import '../login/view.dart';
+
 // import '../search/search_binding.dart';
 // import '../search/search_view.dart';
 // import '../torrent/torrent_binding.dart';
@@ -17,22 +17,22 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.LOGIN;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
-      middlewares: [
-        LoginMiddleware(),
-      ],
+      // middlewares: [
+      //   LoginMiddleware(),
+      // ],
     ),
-    GetPage(
-      name: _Paths.LOGIN,
-      page: () => const LoginView(),
-      binding: LoginBinding(),
-    ),
+    // GetPage(
+    //   name: _Paths.LOGIN,
+    //   page: () => const LoginView(),
+    //   binding: LoginBinding(),
+    // ),
     // GetPage(
     //   name: _Paths.SEARCH,
     //   page: () => const SearchView(),
@@ -46,5 +46,10 @@ class AppPages {
     //   page: () => const TorrentView(),
     //   binding: TorrentBinding(),
     // ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginPage(),
+      binding: LoginBinding(),
+    ),
   ];
 }
