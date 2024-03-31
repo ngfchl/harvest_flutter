00fde63784fa582a0924c133711fdd4428d9f3d0
ngfchl <ngfchl@126.com>
2024-03-31 21:25:17 +0800

    添加工具方法 计算过去了多久

--- lib/utils/calc_weeks.dart
+++ lib/utils/calc_weeks.dart
@@ -7,3 +7,29 @@ String calcWeeksDays(String datetime) {
   }
   return '🔥$weeks周$day天';
 }
+
+String calculateTimeElapsed(String pastTime) {
+  /*
+  计算过去多久了
+   */
+  DateTime currentTime = DateTime.now();
+  DateTime pastDateTime = DateTime.parse(pastTime); // 将传入的字符串解析为 DateTime 对象
+
+  Duration difference = currentTime.difference(pastDateTime); // 计算时间差
+
+  if (difference.inDays > 365) {
+    int years = (difference.inDays / 365).floor();
+    return '$years 年前';
+  } else if (difference.inDays > 30) {
+    int months = (difference.inDays / 30).floor();
+    return '$months 个月前';
+  } else if (difference.inDays > 0) {
+    return '${difference.inDays} 天前';
+  } else if (difference.inHours > 0) {
+    return '${difference.inHours} 小时前';
+  } else if (difference.inMinutes > 0) {
+    return '${difference.inMinutes} 分钟前';
+  } else {
+    return '刚刚';
+  }
+}
