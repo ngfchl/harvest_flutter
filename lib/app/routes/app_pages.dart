a4d05ea3509d2ea376cb286bbcf011b33de92bc2
ngfchl <ngfchl@126.com>
2024-03-31 21:33:58 +0800

    设置目前开启的页面信息

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,14 +1,14 @@
 import 'package:get/get.dart';
 
+import '../../middleware/login_middleware.dart';
 import '../home/home_binding.dart';
 import '../home/home_view.dart';
 import '../login/binding.dart';
 import '../login/view.dart';
-
 // import '../search/search_binding.dart';
 // import '../search/search_view.dart';
-// import '../torrent/torrent_binding.dart';
-// import '../torrent/torrent_view.dart';
+import '../torrent/torrent_binding.dart';
+import '../torrent/torrent_view.dart';
 
 // ignore_for_file: constant_identifier_names
 
@@ -17,22 +17,25 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.LOGIN;
+  static const INITIAL = Routes.HOME;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
-      // middlewares: [
-      //   LoginMiddleware(),
-      // ],
+      middlewares: [
+        LoginMiddleware(),
+      ],
     ),
-    // GetPage(
-    //   name: _Paths.LOGIN,
-    //   page: () => const LoginView(),
-    //   binding: LoginBinding(),
-    // ),
+    GetPage(
+      name: _Paths.TORRENT,
+      page: () => const TorrentView(),
+      binding: TorrentBinding(),
+      middlewares: [
+        LoginMiddleware(),
+      ],
+    ),
     // GetPage(
     //   name: _Paths.SEARCH,
     //   page: () => const SearchView(),
@@ -40,11 +43,6 @@ class AppPages {
     //   middlewares: [
     //     LoginMiddleware(),
     //   ],
-    // ),
-    // GetPage(
-    //   name: _Paths.TORRENT,
-    //   page: () => const TorrentView(),
-    //   binding: TorrentBinding(),
     // ),
     GetPage(
       name: _Paths.LOGIN,
