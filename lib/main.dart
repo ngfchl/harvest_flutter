bfb8a0c2af929fbab4711d76884539c89a4c6aa8
ngfchl <ngfchl@126.com>
2024-03-31 21:24:26 +0800

    调整路由拦截使用中间件来操作

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:harvest/theme/theme.dart';
-import 'package:harvest/utils/logger_helper.dart';
 import 'package:harvest/utils/storage.dart';
 
 import 'app/routes/app_pages.dart';
@@ -21,32 +19,16 @@ void main() async {
   ));
   runApp(
     GetMaterialApp(
-        title: "Harvest",
-        defaultTransition: Transition.cupertino,
-        debugShowCheckedModeBanner: false,
-        initialRoute: AppPages.INITIAL,
-        theme:  ThemeData.light(),
-        darkTheme: ThemeData.dark(),
-        // theme:  lightTheme,
-        // darkTheme: darkTheme,
-        themeMode: ThemeMode.dark,
-        // navigatorKey: Get.key,
-        getPages: AppPages.routes,
-        routingCallback: (routing) {
-          print('当前路由：${routing!.current.toString()}');
-          // todo 先向服务器请求登录状态
-          // 登录状态返回后，再判断是否需要跳转到登录页
-          if (routing.current.toString() == Routes.HOME) {
-            final isLogin = SPUtil.getBool('isLogin');
-            Logger.instance.w(isLogin);
-            Logger.instance.w(Routes.HOME);
-            if (!isLogin!) {
-              String msg = "用户未登录，跳转到登录页！";
-              Logger.instance.w(msg);
-              // Get.snackbar('提示', msg);
-              // Get.toNamed(Routes.LOGIN);
-            }
-          }
-        }),
+      title: "Harvest",
+      defaultTransition: Transition.cupertino,
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppPages.INITIAL,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      // theme: lightTheme,
+      // darkTheme: darkTheme,
+      themeMode: ThemeMode.light,
+      getPages: AppPages.routes,
+    ),
   );
 }
--- lib/middleware/login_middleware.dart
+++ lib/middleware/login_middleware.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import '../app/routes/app_pages.dart';
-import '../utils/storage.dart';
 
 class LoginMiddleware extends GetMiddleware {
   @override
   RouteSettings? redirect(String? route) {
-    print(route);
-    if (SPUtil.getBool('isLogin') == false) {
+    GetStorage box = GetStorage();
+    if (box.read('isLogin') != true && route != Routes.LOGIN) {
       return const RouteSettings(name: Routes.LOGIN);
     }
     return null;
