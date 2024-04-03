75c6e9b1202ee86cb4262c522ae354a19ec41169
ngfchl <ngfchl@126.com>
2024-04-03 21:48:54 +0800

    添加APP 启动时初始化部分数据

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:harvest/utils/dio_util.dart';
 import 'package:harvest/utils/storage.dart';
 
+import 'app/home/home_controller.dart';
+import 'app/home/pages/controller/download_controller.dart';
+import 'app/home/pages/my_site/controller.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
@@ -12,7 +16,13 @@ void main() async {
   await SPUtil.getInstance();
   // 初始化插件前需要在runApp之前调用初始化代码
   WidgetsFlutterBinding.ensureInitialized();
+  // 注册 HomeController 控制器
+  await DioUtil().initialize(GetStorage().read('server'));
+  Get.put(HomeController());
 
+  // 注册 MySiteController 控制器
+  Get.put(MySiteController());
+  Get.put(DownloadController());
   // 固定写法，处理状态栏背景颜色
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
