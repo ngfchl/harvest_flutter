6cf4b1abc4dcb4d1d2589736b152c8653842e40f
ngfchl <ngfchl@126.com>
2024-04-12 01:09:29 +0800

    增加工具方法随机字符串

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -1,7 +1,18 @@
+import 'dart:math';
+
 import 'package:intl/intl.dart';
 
 import '../app/home/pages/models/my_site.dart';
 
 String formatCreatedTimeToDateString(StatusInfo item) {
   return DateFormat("yyyy-MM-dd").format(DateTime.parse(item.createdAt));
+}
+
+String generateRandomString(int length) {
+  const charset = 'abcdefghijklmnopqrstuvwxyz0123456789¥#%@&*+-_^';
+  Random random = Random();
+  return String.fromCharCodes(
+    List.generate(
+        length, (_) => charset.codeUnitAt(random.nextInt(charset.length))),
+  );
 }
