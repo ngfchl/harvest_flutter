b5ecb7d9790862c9133d11f1ad70cb7b87b3f0dd
ngfchl <ngfchl@126.com>
2024-04-18 23:23:49 +0800

    初步完成订阅标签的增删改查

--- lib/app/home/pages/subscribe_tag/controller.dart
+++ lib/app/home/pages/subscribe_tag/controller.dart
@@ -1,6 +1,47 @@
 import 'package:get/get.dart';
+import 'package:harvest/app/home/pages/models/SubTag.dart';
+import 'package:harvest/models/common_response.dart';
+
+import '../../../../api/sub.dart';
+import '../../../../common/meta_item.dart';
 
 class SubscribeTagController extends GetxController {
+  List<SubTag> tags = [];
+
+  List<MetaDataItem> tagCategoryList = [
+    {'name': '剧集', 'value': 'season'},
+    {'name': '发布年份', 'value': 'publish_year'},
+    {'name': '优惠折扣', 'value': 'discount'},
+    {'name': '视频质量', 'value': 'video_quality'},
+    {'name': '分辨率', 'value': 'resolution'},
+    {'name': '视频编码', 'value': 'video_codec'},
+    {'name': '音频编码', 'value': 'audio_codec'},
+    {'name': '发行方', 'value': 'publisher'},
+    {'name': '种子标签', 'value': 'tags'},
+    {'name': '资源来源', 'value': 'source'},
+    {'name': '资源分类', 'value': 'category'},
+  ].map((e) => MetaDataItem.fromJson(e)).toList();
+
+  initData() async {
+    await getTagsFromServer();
+  }
+
+  getTagsFromServer() async {
+    CommonResponse response = await getTagListApi();
+    if (response.code == 0) {
+      tags = response.data;
+    } else {
+      Get.snackbar('订阅标签获取失败', "订阅标签获取失败");
+    }
+    update();
+  }
+
+  @override
+  void onInit() async {
+    await initData();
+    super.onInit();
+  }
+
   @override
   void onReady() {
     // TODO: implement onReady
@@ -11,5 +52,26 @@ class SubscribeTagController extends GetxController {
   void onClose() {
     // TODO: implement onClose
     super.onClose();
+  }
+
+  saveSubTag(SubTag tag) async {
+    CommonResponse res;
+    if (tag.id == 0) {
+      res = await addSubTagApi(tag);
+    } else {
+      res = await editSubTagApi(tag);
+    }
+    if (res.code == 0) {
+      await getTagsFromServer();
+    }
+    return res;
+  }
+
+  removeSubTag(SubTag tag) async {
+    CommonResponse res = await removeSubTagApi(tag);
+    if (res.code == 0) {
+      await getTagsFromServer();
+    }
+    return res;
   }
 }
--- lib/app/home/pages/subscribe_tag/view.dart
+++ lib/app/home/pages/subscribe_tag/view.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:harvest/common/card_view.dart';
+import 'package:harvest/models/common_response.dart';
 
+import '../../../../common/form_widgets.dart';
+import '../../../../common/meta_item.dart';
+import '../../../../utils/logger_helper.dart';
+import '../models/SubTag.dart';
 import 'controller.dart';
 
 class SubscribeTagPage extends StatefulWidget {
@@ -15,12 +23,277 @@ class _SubscribeTagPageState extends State<SubscribeTagPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("订阅标签");
+    return GetBuilder<SubscribeTagController>(builder: (controller) {
+      return Column(
+        children: [
+          CustomCard(
+            child: ListTile(
+              dense: true,
+              title: const Text(
+                '订阅标签',
+                style: TextStyle(fontSize: 18, color: Colors.black87),
+              ),
+              leading: IconButton(
+                  onPressed: () => controller.getTagsFromServer(),
+                  icon: const Icon(
+                    Icons.refresh,
+                    color: Colors.green,
+                  )),
+              trailing: IconButton(
+                icon: const Icon(
+                  Icons.add,
+                  size: 28,
+                  color: Colors.blue,
+                ),
+                onPressed: () {
+                  _openEditDialog(null);
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: GetBuilder<SubscribeTagController>(builder: (controller) {
+              return SingleChildScrollView(
+                child: Wrap(
+                  children: controller.tags
+                      .map((SubTag tag) => _buildTag(tag))
+                      .toList(),
+                ),
+              );
+            }),
+          ),
+          const SizedBox(height: 60),
+        ],
+      );
+    });
   }
 
   @override
   void dispose() {
     Get.delete<SubscribeTagController>();
     super.dispose();
+  }
+
+  CustomCard _buildTag(SubTag tag) {
+    return CustomCard(
+      child: Slidable(
+        key: ValueKey('${tag.id}_${tag.name}'),
+        startActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              onPressed: (context) async {
+                _openEditDialog(tag);
+              },
+              flex: 1,
+              backgroundColor: const Color(0xFF0392CF),
+              foregroundColor: Colors.white,
+              icon: Icons.edit,
+              label: '编辑',
+            ),
+          ],
+        ),
+        endActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              onPressed: (context) async {
+                Get.defaultDialog(
+                  title: '确认',
+                  backgroundColor: Colors.white,
+                  radius: 5,
+                  titleStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.deepPurple),
+                  middleText: '确定要删除标签吗？',
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Get.back(result: false);
+                      },
+                      child: const Text('取消'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        Get.back(result: true);
+                        CommonResponse res = await controller.removeSubTag(tag);
+                        if (res.code == 0) {
+                          Get.snackbar('删除通知', res.msg.toString(),
+                              backgroundColor: Colors.green.shade500,
+                              colorText: Colors.white70);
+                        } else {
+                          Get.snackbar('删除通知', res.msg.toString(),
+                              backgroundColor: Colors.red.shade500,
+                              colorText: Colors.white70);
+                        }
+                      },
+                      child: const Text('确认'),
+                    ),
+                  ],
+                );
+              },
+              flex: 1,
+              backgroundColor: const Color(0xFFFE4A49),
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: '删除',
+            ),
+          ],
+        ),
+
+        // The end action pane is the one at the right or the bottom side.
+        child: ListTile(
+          dense: true,
+          title: Text(tag.name!),
+          subtitle: Text(
+            tag.category!,
+            style: const TextStyle(fontSize: 10, color: Colors.black54),
+          ),
+          onTap: () => _openEditDialog(tag),
+          trailing: IconButton(
+            icon: tag.available == true
+                ? const Icon(
+                    Icons.check_box,
+                    size: 18,
+                    color: Colors.green,
+                  )
+                : const Icon(
+                    Icons.disabled_by_default,
+                    size: 18,
+                    color: Colors.red,
+                  ),
+            onPressed: () async {
+              SubTag newTag = tag.copyWith(available: !tag.available!);
+              submitForm(newTag);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  _openEditDialog(SubTag? tag) {
+    '''
+    String? name,
+    String? category,
+    bool? available,
+    ''';
+    final TextEditingController nameController =
+        TextEditingController(text: tag != null ? tag.name : '');
+    final TextEditingController categoryController = TextEditingController(
+        text: tag != null ? tag.category : controller.tagCategoryList[0].value);
+
+    RxBool available = true.obs;
+    final isLoading = false.obs;
+
+    available.value = tag != null ? tag.available! : true;
+    String title = tag != null ? '编辑标签：${tag.name!}' : '添加订阅标签';
+    Get.defaultDialog(
+        title: title,
+        content: Column(
+          children: [
+            CustomTextField(
+              controller: nameController,
+              labelText: '名称',
+            ),
+            DropdownButtonFormField<String>(
+              value: categoryController.text,
+              onChanged: (String? newValue) {
+                categoryController.text = newValue!;
+              },
+              items: controller.tagCategoryList
+                  .map<DropdownMenuItem<String>>(
+                      (MetaDataItem item) => DropdownMenuItem<String>(
+                            value: item.value,
+                            child: Center(child: Text(item.name)),
+                          ))
+                  .toList(),
+            ),
+            Obx(() {
+              return SwitchListTile(
+                  dense: true,
+                  contentPadding: EdgeInsets.zero,
+                  title: const Text(
+                    '标签可用',
+                    style: TextStyle(fontSize: 12, color: Colors.black54),
+                  ),
+                  value: available.value,
+                  onChanged: (bool val) {
+                    available.value = val;
+                  });
+            }),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // 清空表单数据
+                    nameController.clear();
+                    categoryController.clear();
+                    available.value = true;
+                    Get.back();
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        Colors.redAccent.withAlpha(150)),
+                  ),
+                  icon: const Icon(Icons.cancel_outlined, color: Colors.white),
+                  label: const Text(
+                    '取消',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Obx(() {
+                  return ElevatedButton.icon(
+                    onPressed: () async {
+                      isLoading.value = true;
+                      SubTag newTag;
+                      if (tag != null) {
+                        newTag = tag.copyWith(
+                          name: nameController.text,
+                          category: categoryController.text,
+                          available: available.value,
+                        );
+                      } else {
+                        newTag = SubTag.fromJson({
+                          'id': 0,
+                          'name': nameController.text,
+                          'category': categoryController.text,
+                          'available': available.value,
+                        });
+                      }
+                      submitForm(newTag);
+                      isLoading.value = false;
+                    },
+                    icon: isLoading.value
+                        ? const GFLoader(size: 18)
+                        : const Icon(Icons.save),
+                    label: const Text('保存'),
+                  );
+                }),
+              ],
+            ),
+          ],
+        ));
+  }
+
+  void submitForm(SubTag tag) async {
+    try {
+      CommonResponse res = await controller.saveSubTag(tag);
+
+      Logger.instance.i(res.msg);
+      if (res.code == 0) {
+        Get.back();
+        Get.snackbar('标签保存成功！', res.msg!,
+            backgroundColor: Colors.green.shade300, colorText: Colors.white);
+      } else {
+        Get.snackbar('标签保存失败！', res.msg!,
+            backgroundColor: Colors.red.shade300, colorText: Colors.white);
+      }
+    } finally {}
   }
 }
