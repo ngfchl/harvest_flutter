93c7efced8204a79a64b42d1b7a6ff9ad64b5729
ngfchl <ngfchl@126.com>
2024-04-20 10:45:01 +0800

    增加日期比较方法

--- lib/utils/date_time_utils.dart
+++ lib/utils/date_time_utils.dart
@@ -6,6 +6,24 @@ String formatTimestampToDateTime(int timestamp,
     DateFormat(format)
         .format(DateTime.fromMillisecondsSinceEpoch(timestamp * 1000));
 
+/// 判断两个日期时间是否是同一天
+bool isSameDay(DateTime date1, DateTime date2) {
+  return date1.year == date2.year &&
+      date1.month == date2.month &&
+      date1.day == date2.day;
+}
+
+/// 是否今日
+bool isToday(String dateString) {
+  final DateTime parsedDate = DateTime.parse(dateString);
+  final today = DateTime.now().subtract(Duration(
+      hours: DateTime.now().timeZoneOffset.inHours)); // 考虑到时区，获取本地今天的日期（不考虑时间）
+
+  return parsedDate.year == today.year &&
+      parsedDate.month == today.month &&
+      parsedDate.day == today.day;
+}
+
 /// 时长处理
 String formatDuration(int seconds) {
   Duration duration = Duration(seconds: seconds);
