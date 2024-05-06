575a0a9cdd60b238c92b52c3a1c99b145c8ae4de
ngfchl <ngfchl@126.com>
2024-05-07 01:41:59 +0800

    调整启动代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,31 @@
 import 'package:app_service/app_service.dart';
+import 'package:app_service/src/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:harvest/utils/dio_util.dart';
 import 'package:harvest/utils/storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app/routes/app_pages.dart';
+import 'app_service_basic/app_service.dart';
 import 'app_service_basic/get_it_injections.dart';
+import 'app_service_basic/prefs.dart';
 
 void main() async {
-  await GetStorage.init();
+  // 初始化插件前需要在runApp之前调用初始化代码
+  WidgetsFlutterBinding.ensureInitialized();
   // 初始化 持久化数据信息
   await SPUtil.getInstance();
   GetItInjection.init();
+  Get.testMode = false;
 
-  // 初始化插件前需要在runApp之前调用初始化代码
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  initialization(null);
   // 注册 HomeController 控制器
-  String? server = GetStorage().read('server');
+  String? server = SPUtil.getLocalStorage('server');
   if (server == null || server.length <= 10) {
-    GetStorage().write('isLogin', false);
+    SPUtil.setBool('isLogin', false);
   } else {
     await DioUtil().initialize(server);
   }
@@ -36,30 +38,55 @@ void main() async {
     statusBarColor: Colors.transparent,
   ));
   FlutterNativeSplash.remove();
-  runApp(
-    GetMaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  Future<void> onInit(BuildContext context) async {
+    final appService = GetIt.instance.get<AppService>();
+    await appService.init();
+    //延迟3秒
+    await Future.delayed(const Duration(seconds: 3));
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appService = GetIt.instance.get<AppService>();
+    String? currentTheme = SPUtil.getString('themeEnum',
+        defaultValue: ColorThemesEnum.brownOrange.name);
+    appService.isDarkMode.value = Get.isDarkMode;
+    return GetMaterialApp(
       title: "Harvest",
       defaultTransition: Transition.cupertino,
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: Get.testMode,
       initialRoute: AppPages.INITIAL,
       navigatorKey: Get.key,
-      // themeMode: ThemeMode.system,
+      theme: appService.currentTheme,
+      darkTheme: getThemeDataByName(currentTheme!, true),
+      themeMode: ThemeMode.system,
       getPages: AppPages.routes,
+      translations: Messages([
+        AppServiceMessages().keys,
+      ]),
+      locale: const Locale('zh', 'CN'),
+      fallbackLocale: const Locale('en', 'US'),
       onInit: () async {
-        await onInit();
+        await onInit(context);
       },
-    ),
-  );
+    );
+  }
 }
 
-//启动图延时移除方法
-void initialization(BuildContext? context) async {
-  //延迟3秒
-  await Future.delayed(const Duration(seconds: 3));
-  FlutterNativeSplash.remove();
-}
+/// 基于 Get it 库的依赖注入
+class GetitInjection {
+  static void init() {
+    final GetIt i = GetIt.instance;
 
-Future<void> onInit() async {
-  final appService = GetIt.instance.get<AppService>();
-  await appService.init();
+    i.registerSingletonAsync<SharedPreferences>(() => prefsInstance());
+
+    i.registerLazySingleton<AppService>(() => appService(i)); // 应用基础服务
+  }
 }
