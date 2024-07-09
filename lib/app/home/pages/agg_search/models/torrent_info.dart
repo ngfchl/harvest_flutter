0a374bd77e4feb57bd358071691827da1d96cef7
ngfchl <ngfchl@126.com>
2024-07-09 18:07:04 +0800

    种子搜索结果增加tag 字段

--- lib/app/home/pages/agg_search/models/torrent_info.dart
+++ lib/app/home/pages/agg_search/models/torrent_info.dart
@@ -8,6 +8,7 @@ class SearchTorrentInfo {
   String title;
   String subtitle;
   String saleStatus;
+  List<String> tags;
   double? progress;
   DateTime? saleExpire;
   bool hr;
@@ -29,6 +30,7 @@ class SearchTorrentInfo {
     required this.saleStatus,
     this.saleExpire,
     this.progress,
+    required this.tags,
     required this.hr,
     required this.published,
     required this.size,
@@ -39,10 +41,16 @@ class SearchTorrentInfo {
 
   factory SearchTorrentInfo.fromJson(Map<String, dynamic> json) {
     dynamic published;
+    dynamic size;
     try {
       published = DateTime.parse(json['published']);
     } catch (e) {
       published = json['published'];
+    }
+    try {
+      size = int.parse(json['size']);
+    } catch (e) {
+      size = 0;
     }
 
     return SearchTorrentInfo(
@@ -55,6 +63,7 @@ class SearchTorrentInfo {
       title: json['title'],
       subtitle: json['subtitle'] ?? '',
       progress: json['progress'],
+      tags: List<String>.from(json['tags']),
       saleStatus: json['sale_status'] ?? '无优惠',
       saleExpire: json['sale_expire'] != null
           ? DateTime.parse(json['sale_expire'])
@@ -80,9 +89,9 @@ class SearchTorrentInfo {
     data['subtitle'] = subtitle;
     data['progress'] = progress;
     data['sale_status'] = saleStatus;
-    data['sale_expire'] = saleExpire?.toIso8601String();
+    data['sale_expire'] = saleExpire?.toString();
     data['hr'] = hr;
-    data['published'] = published.toIso8601String();
+    data['published'] = published.toString();
     data['size'] = size;
     data['seeders'] = seeders;
     data['leechers'] = leechers;
@@ -100,6 +109,7 @@ class SearchTorrentInfo {
     String? title,
     String? subtitle,
     String? saleStatus,
+    List<String>? tags,
     DateTime? saleExpire,
     double? progress,
     bool? hr,
@@ -122,6 +132,7 @@ class SearchTorrentInfo {
       saleStatus: saleStatus ?? this.saleStatus,
       saleExpire: saleExpire ?? this.saleExpire,
       hr: hr ?? this.hr,
+      tags: tags ?? this.tags,
       published: published ?? this.published,
       size: size ?? this.size,
       seeders: seeders ?? this.seeders,
