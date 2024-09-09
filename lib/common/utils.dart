040d4d5235ea4885fb702817a17ab0885a0e3a3c
ngfchl <ngfchl@gmail.com>
2024-09-09 12:13:14 +0800

    优化随机字符串生成方法，支持自定义是否包含数字和特殊字符

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -8,12 +8,30 @@ String formatCreatedTimeToDateString(StatusInfo item) {
   return DateFormat("MM-dd").format(item.createdAt);
 }
 
-String generateRandomString(int length) {
-  const charset = 'abcdefghijklmnopqrstuvwxyz0123456789¥#%@&*+-_^';
+String generateRandomString(
+  int length, {
+  bool includeNumbers = true,
+  bool includeSpecialChars = true,
+  bool includeUppercase = true,
+}) {
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const numbers = '0123456789';
+  const specialChars = '¥#%@&*+-_^';
+
+  // 基础字符集为小写字母
+  String charset = lowercaseChars;
+
+  // 根据参数条件添加其他字符集
+  if (includeNumbers) charset += numbers;
+  if (includeSpecialChars) charset += specialChars;
+  if (includeUppercase) charset += lowercaseChars.toUpperCase();
+
   Random random = Random();
   return String.fromCharCodes(
     List.generate(
-        length, (_) => charset.codeUnitAt(random.nextInt(charset.length))),
+      length,
+      (_) => charset.codeUnitAt(random.nextInt(charset.length)),
+    ),
   );
 }
 
