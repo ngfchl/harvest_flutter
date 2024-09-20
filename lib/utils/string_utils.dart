4bc7cdb14f20796023317474ebca9c469e80c4d3
ngfchl <ngfchl@gmail.com>
2024-09-20 10:25:34 +0800

    判断一个字符串能否解析成 json，能的话返回 json，不能的返回字符串

--- lib/utils/string_utils.dart
+++ lib/utils/string_utils.dart
@@ -1,6 +1,18 @@
+import 'dart:convert';
+
 class StringUtils {
   static String getLottieByName(String lotterName) {
     return "assets/lotties/$lotterName.json";
+  }
+
+  static dynamic parseJsonOrReturnString(String str) {
+    try {
+      // 尝试解析字符串为 JSON
+      return jsonDecode(str);
+    } on FormatException {
+      // 如果解析失败，返回原始字符串
+      return str;
+    }
   }
 }
 
