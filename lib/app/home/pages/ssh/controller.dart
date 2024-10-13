b755b4bceae3df0c0f15079db81a034f9002c97f
ngfchl <ngfchl@gmail.com>
2024-10-13 18:32:07 +0800

    update. 1. 增加代理 2. 增加记住密码 3. 增加镜像更新状态检查 4. 优化重建命令生成并完成镜像重建功能 5. 优化日志

--- lib/app/home/pages/ssh/controller.dart
+++ lib/app/home/pages/ssh/controller.dart
@@ -3,15 +3,15 @@ import 'dart:convert';
 import 'package:dartssh2_plus/dartssh2.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:logger/logger.dart';
 
+import '../../../../utils/logger_helper.dart';
 import '../../../../utils/storage.dart';
 import 'models.dart';
 
-Logger logger = Logger();
-
 class SshController extends GetxController {
   bool connected = false;
+  bool remember = false;
+  bool haveNewImage = false;
   late SSHClient client;
   TextEditingController usernameController =
       TextEditingController(text: 'root');
@@ -19,6 +19,7 @@ class SshController extends GetxController {
   TextEditingController hostController =
       TextEditingController(text: '192.168.1.1');
   TextEditingController portController = TextEditingController(text: '22');
+  TextEditingController proxyController = TextEditingController(text: '');
   List<String> results = ['SSHClient欢迎你！'];
   List<DockerContainer> containerList = [];
 
@@ -26,9 +27,13 @@ class SshController extends GetxController {
   void onInit() {
     String? serverDomain =
         SPUtil.getString('serverDomain', defaultValue: '192.168.1.1');
-
+    String? password =
+        SPUtil.getString('SSH_CLIENT_PASSWORD', defaultValue: '');
+    String? proxy = SPUtil.getString('SSH_CLIENT_PASSWORD', defaultValue: '');
     hostController.text = serverDomain!;
-
+    passwordController.text = password!;
+    proxyController.text = proxy!;
+    remember = SPUtil.getBool('SSH_CLIENT_REMEMBER', defaultValue: false)!;
     super.onInit();
   }
 
@@ -40,8 +45,12 @@ class SshController extends GetxController {
         username: usernameController.text,
         onPasswordRequest: () => passwordController.text,
       );
-
-      logger.i(await run('whoami'));
+      if (remember) {
+        SPUtil.setLocalStorage('SSH_CLIENT_PASSWORD', passwordController.text);
+        SPUtil.setLocalStorage('SSH_CLIENT_PROXY', proxyController.text);
+        SPUtil.setBool('SSH_CLIENT_REMEMBER', remember);
+      }
+      Logger.instance.i(await run('whoami'));
       String msg;
       if (client.isClosed) {
         msg = 'SSH 连接到 ${hostController.text} 失败！';
@@ -50,11 +59,11 @@ class SshController extends GetxController {
         msg = 'SSH 连接到 ${hostController.text}';
         await getContainerList();
       }
-      logger.i(msg);
+      Logger.instance.i(msg);
       results.add(msg);
     } catch (e, trace) {
-      logger.e(e);
-      logger.e(trace);
+      Logger.instance.e(e);
+      Logger.instance.e(trace);
     }
     update();
   }
@@ -68,14 +77,15 @@ class SshController extends GetxController {
   generateNewContainerCommand(String name) async {
     String command = "docker inspect --format='docker run -d "
         "--name {{(slice .Name 1)}} {{if .HostConfig.Privileged}}--privileged {{end}}"
-        "--network {{.HostConfig.NetworkMode}} "
         "{{range \$key, \$value := .Config.Labels}}--label {{\$key}}=\"{{\$value}}\" {{end}} "
         "{{range \$index, \$value := .Config.Env}}--env {{\$value}} {{end}} "
         "{{range \$index, \$value := .HostConfig.PortBindings}}-p {{(index \$value 0).HostPort}}:{{(index \$value 0).HostPort}} "
         "{{end}} {{range \$index, \$value := .Mounts}}-v {{\$value.Source}}:{{\$value.Destination}} {{end}} "
-        "{{.Config.Image}}'  $name";
+        "{{range \$network, \$details := .NetworkSettings.Networks}}--network {{\$network}} {{if \$details.IPAddress}}--ip {{\$details.IPAddress}} {{end}} {{end}} "
+        "{{.Config.Image}}' $name";
+
     String newCommand = await run(command);
-    logger.i(newCommand);
+    Logger.instance.i(newCommand);
     results.add(newCommand);
     update();
     return newCommand;
@@ -87,13 +97,11 @@ class SshController extends GetxController {
   }
 
   execute(String command) async {
-    clear();
-    update();
     SSHSession session = await client.execute(command);
     // 监听 stdout
     session.stdout.listen((data) {
       final output = utf8.decode(data);
-      logger.i('Output: $output');
+      Logger.instance.i('Output: $output');
       results.add(output);
 
       update();
@@ -102,7 +110,7 @@ class SshController extends GetxController {
     // 监听 stderr
     session.stderr.listen((data) {
       final errorOutput = utf8.decode(data);
-      logger.i('Error: $errorOutput');
+      Logger.instance.i('Error: $errorOutput');
       results.add(errorOutput);
 
       update();
@@ -117,11 +125,15 @@ class SshController extends GetxController {
   }
 
   getContainerList() async {
+    Logger.instance.i('开始获取容器列表');
     String command =
         'docker ps -a --format "{{.ID}} {{.Image}} {{.Names}} {{.Status}}"';
+    clear();
+    results.add('开始获取容器列表');
+    update();
     SSHSession session = await client.execute(command);
     final output = await session.stdout.map(utf8.decode).join();
-    print(output);
+    Logger.instance.d(output);
     containerList = output
         .trim()
         .split('\n')
@@ -136,41 +148,88 @@ class SshController extends GetxController {
         })
         .map((e) => DockerContainer.fromJson(e))
         .toList();
-    logger.i('Output: $containerList'); // 期望输出 Docker 容器列表
+    Logger.instance.i('Output: $containerList'); // 期望输出 Docker 容器列表
     update();
   }
 
-  @override
-  void onReady() {
-    // TODO: implement onReady
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    // TODO: implement onClose
-    super.onClose();
-  }
-
   void restartContainer(String? name) async {
+    Logger.instance.i('正在重启容器：$name');
+    clear();
+    results.add('正在重启容器：$name');
+    update();
     String command = 'docker restart $name';
     await execute(command);
     await getContainerList();
     update();
   }
 
+  getImageCreatedTime(String image) async {
+    String command = "docker inspect --format='{{.Created}}' $image";
+    String createdTime = await run(command);
+    return createdTime;
+  }
+
+  getImageLastUpdated(String image) async {
+    String tag = 'latest';
+    String name = '';
+    if (image.contains(':')) {
+      name = image.split(':').first;
+      tag = image.split(':').last;
+    } else {
+      name = image;
+    }
+    if (!name.contains('/')) {
+      name = 'library/$name';
+    }
+    List<String> parts = name.split('/');
+    name = '${parts[parts.length - 2]}/${parts[parts.length - 1]}';
+    results.add('镜像名称：$name:$tag');
+    update();
+
+    String command = 'curl ';
+    if (proxyController.text.isNotEmpty) {
+      command += '-x ${proxyController.text} ';
+    }
+    command += """
+        -s https://hub.docker.com/v2/repositories/$name/tags/$tag/ | grep '"last_updated"' | sed 's/.*"last_updated":"\\([^"]*\\)".*/\\1/'
+        """;
+
+    results.add(command);
+    return await run(command);
+  }
+
+  checkNewImage(String image) async {
+    clear();
+    results.add('检查更新：$image');
+    update();
+    String createdTime = await getImageCreatedTime(image);
+    results.add('当前镜像创建时间：$createdTime');
+
+    String updatedTime = await getImageLastUpdated(image);
+    results.add('远程镜像更新时间：$updatedTime');
+
+    if (updatedTime.compareTo(createdTime) > 0) {
+      haveNewImage = true;
+    }
+    update();
+  }
+
   void getNewImage(String? image) async {
     String command = 'docker pull $image';
+    clear();
+    results.add('正在更新镜像：$image');
+    update();
     await execute(command);
     update();
   }
 
   void rebuildContainer(String name, String image) async {
-    String command = 'docker stop $name';
-    await execute(command);
-    command = 'docker commit $name $image';
-    await execute(command);
-    command = 'docker commit $name $image';
+    Logger.instance.i('正在重建容器：$name');
+
+    String newCommand = await generateNewContainerCommand(name);
+
+    String command =
+        'docker pull $image && docker stop $name && docker rm -f $name && $newCommand';
     await execute(command);
     await getContainerList();
     update();
--- lib/app/home/pages/ssh/view.dart
+++ lib/app/home/pages/ssh/view.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:harvest/common/card_view.dart';
 import 'package:harvest/common/form_widgets.dart';
 
+import '../../../../utils/logger_helper.dart';
 import 'controller.dart';
 import 'models.dart';
 
@@ -19,9 +20,6 @@ class SshWidget extends StatelessWidget {
 
     return GetBuilder<SshController>(builder: (controller) {
       return Scaffold(
-        appBar: AppBar(
-          title: const Text('SSH Client'),
-        ),
         body: controller.connected
             ? Column(
                 children: [
@@ -129,10 +127,11 @@ class SshWidget extends StatelessWidget {
                                               ),
                                               InkWell(
                                                   onTap: () =>
-                                                      controller.getNewImage(
-                                                          container.image),
+                                                      controller.checkNewImage(
+                                                          container.image
+                                                              .toString()),
                                                   child: const Text(
-                                                    '拉取最新镜像',
+                                                    '检查更新',
                                                     style:
                                                         TextStyle(fontSize: 11),
                                                   )),
@@ -148,39 +147,74 @@ class SshWidget extends StatelessWidget {
                                         mainAxisAlignment:
                                             MainAxisAlignment.spaceAround,
                                         children: [
-                                          GestureDetector(
-                                            onTap: () async {
+                                          IconButton(
+                                            constraints: const BoxConstraints(
+                                              maxHeight: 28,
+                                              maxWidth: 28,
+                                            ),
+                                            padding: const EdgeInsets.all(5),
+                                            onPressed: () async {
                                               await controller.stopContainer(
                                                   container.name.toString());
                                             },
-                                            child: const Icon(
+                                            icon: const Icon(
                                               Icons.stop_circle_outlined,
-                                              size: 15,
+                                              size: 13,
                                             ),
                                           ),
-                                          const SizedBox(height: 10),
-                                          GestureDetector(
-                                            onTap: () =>
+                                          IconButton(
+                                            constraints: const BoxConstraints(
+                                              maxHeight: 28,
+                                              maxWidth: 28,
+                                            ),
+                                            padding: const EdgeInsets.all(5),
+                                            onPressed: () =>
                                                 controller.restartContainer(
                                                     container.name.toString()),
-                                            child: const Icon(
+                                            icon: const Icon(
                                               Icons.refresh_outlined,
-                                              size: 15,
+                                              size: 13,
                                             ),
                                           ),
-                                          const SizedBox(height: 10),
-                                          GestureDetector(
-                                            onTap: () => controller
-                                                .generateNewContainerCommand(
-                                                    container.name.toString()),
-                                            child: const Icon(
+                                          IconButton(
+                                            padding: const EdgeInsets.all(5),
+                                            constraints: const BoxConstraints(
+                                              maxHeight: 28,
+                                              maxWidth: 28,
+                                            ),
+                                            onPressed: () {
+                                              Get.defaultDialog(
+                                                title: '重建容器',
+                                                middleText:
+                                                    '本操作会删除旧容器并使用原来的配置重建容器，具有一定的风险性，请谨慎操作',
+                                                middleTextStyle: TextStyle(
+                                                    color: Theme.of(context)
+                                                        .colorScheme
+                                                        .error),
+                                                onConfirm: () {
+                                                  controller.rebuildContainer(
+                                                      container.name.toString(),
+                                                      container.image
+                                                          .toString());
+                                                  Get.back();
+                                                },
+                                                onCancel: () => Get.back(),
+                                                textCancel: '取消',
+                                                textConfirm: '继续',
+                                              );
+                                            },
+                                            icon: const Icon(
                                               Icons.upload_outlined,
-                                              size: 15,
+                                              size: 13,
                                             ),
                                           ),
-                                          const SizedBox(height: 10),
-                                          GestureDetector(
-                                            onTap: () async {
+                                          IconButton(
+                                            padding: const EdgeInsets.all(5),
+                                            constraints: const BoxConstraints(
+                                              maxHeight: 28,
+                                              maxWidth: 28,
+                                            ),
+                                            onPressed: () async {
                                               String newCommand = await controller
                                                   .generateNewContainerCommand(
                                                       container.name
@@ -189,12 +223,12 @@ class SshWidget extends StatelessWidget {
                                                       text: newCommand))
                                                   .then((_) {
                                                 // 可选：在复制后显示消息
-                                                logger.i('文本已复制到剪切板');
+                                                Logger.instance.i('文本已复制到剪切板');
                                               });
                                             },
-                                            child: const Icon(
+                                            icon: const Icon(
                                               Icons.copy_outlined,
-                                              size: 15,
+                                              size: 13,
                                             ),
                                           ),
                                         ],
@@ -203,7 +237,7 @@ class SshWidget extends StatelessWidget {
                                   ],
                                 ));
                           })),
-                  SizedBox(
+                  CustomCard(
                       height: MediaQuery.of(context).size.height * 0.2,
                       child: ListView.builder(
                           itemCount: controller.results.length,
@@ -221,7 +255,18 @@ class SshWidget extends StatelessWidget {
                 ],
               )
             : Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  AppBar(
+                    title: const Text('SSH Client'),
+                  ),
+                  const CustomTextTag(
+                    labelText: 'SSH 连接设备为危险操作，请你一直要只考每一个 SSH 命令的含义确认操作',
+                    backgroundColor: Colors.orange,
+                  ),
+                  const Text(
+                    '本功能仅支持使用 root 账户登录或者不使用 root账户可以直接操作的设备。已知不可用设备：群晖、极空间，',
+                  ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextField(
@@ -265,6 +310,26 @@ class SshWidget extends StatelessWidget {
                   ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: controller.proxyController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: '默认代理',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SwitchListTile(
+                        title: const Text('记住密码'),
+                        value: controller.remember,
+                        onChanged: (bool value) {
+                          controller.remember = value;
+                          controller.update();
+                        }),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
                     child: FullWidthButton(
                       labelColor: Theme.of(context).colorScheme.onPrimary,
                       backgroundColor: Theme.of(context).colorScheme.primary,
@@ -275,8 +340,8 @@ class SshWidget extends StatelessWidget {
                             Get.snackbar('登录失败！', '登录失败啦，请检查用户名密码！');
                           }
                         } catch (e, trace) {
-                          logger.e(e);
-                          logger.e(trace);
+                          Logger.instance.e(e);
+                          Logger.instance.e(trace);
                         }
                       },
                       text: '连接',
