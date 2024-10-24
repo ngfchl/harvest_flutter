8e0c9e684103c77566c34ad2bf4cd902fefcb434
ngfchl <ngfchl@gmail.com>
2024-10-24 12:20:10 +0800

    add. 自定义字节与文件大小转换方法

--- lib/utils/string_utils.dart
+++ lib/utils/string_utils.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 class StringUtils {
   static String getLottieByName(String lotterName) {
@@ -18,4 +19,45 @@ class StringUtils {
 
 String capitalize(String str) {
   return str.isNotEmpty ? '${str[0].toUpperCase()}${str.substring(1)}' : str;
+}
+
+class FileSizeConvert {
+  /// 将文件大小字符串解析为字节
+  static int parseToByte(String fileSize) {
+    if (fileSize.isEmpty) {
+      return 0;
+    }
+
+    // 正则表达式来匹配文件大小和单位
+    RegExp regex =
+        RegExp(r'(\d+(?:\.\d+)?)\s*([kmgtp]?b)', caseSensitive: false);
+
+    List<String> order = ['b', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb'];
+    fileSize = fileSize.replaceAll("i", ""); // 移除 "i" 标志 (例如GiB -> GB)
+
+    for (final match in regex.allMatches(fileSize)) {
+      double value = double.parse(match.group(1)!);
+      String unit = match.group(2)!.toLowerCase();
+      return (value * pow(1024, order.indexOf(unit))).toInt();
+    }
+    return 0;
+  }
+
+  /// 将字节数转换为文件大小字符串
+  static String parseToFileSize(int byte) {
+    if (byte == 0) {
+      return '0B';
+    }
+
+    List<String> units = ["B", "KB", "MB", "GB", "TB", "PB", 'EB'];
+    double size = 1024.0;
+
+    for (int i = 0; i < units.length; i++) {
+      if (byte / size < 1) {
+        return "${byte.toStringAsFixed(3)} ${units[i]}";
+      }
+      byte = (byte / size).round();
+    }
+    return "${byte.toStringAsFixed(3)} EB";
+  }
 }
