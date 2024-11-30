12602d06be535314c90c62ba3d8c94dcb181259e
ngfchl <ngfchl@gmail.com>
2024-11-30 22:00:49 +0800

    update. 长数字处理增加小数点后位数设置

--- lib/utils/format_number.dart
+++ lib/utils/format_number.dart
@@ -1,9 +1,9 @@
-String formatNumber(num value) {
+String formatNumber(num value, {int fixed = 2}) {
   List<String> sizes = ['', 'W', 'E'];
   int i = 0;
   while (value >= 10000 && i < sizes.length - 1) {
     value /= 10000;
     i++;
   }
-  return '${value.toStringAsFixed(2)}${sizes[i]}';
+  return '${value.toStringAsFixed(fixed)}${sizes[i]}';
 }
