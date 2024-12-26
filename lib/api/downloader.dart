bbf91206acf55b09ffca7eef890158ee05b2af1f
ngfchl <ngfchl@gmail.com>
2024-12-26 22:56:24 +0800

    add. 添加 Docker 操作下载器 Api 方法

--- lib/api/downloader.dart
+++ lib/api/downloader.dart
@@ -1,4 +1,5 @@
 import 'package:harvest/api/hooks.dart';
+import 'package:qbittorrent_api/qbittorrent_api.dart';
 
 import '../models/common_response.dart';
 import '../models/download.dart';
@@ -56,22 +57,141 @@ removeDownloaderApi(Downloader downloader) async {
   return await removeData(apiUrl);
 }
 
+//*///@title 获取下载器全量数据
+///@description
+///@updateTime
+Future<CommonResponse> getMainData(int downloaderId) async {
+  final response = await fetchBasicData('${Api.DOWNLOADER_MAIN}$downloaderId');
+  return response;
+}
+
+//*///@title 获取下载器种子详情
+///@description
+///@updateTime
+Future<CommonResponse> getTorrentDetailInfo(
+    int downloaderId, String torrentHash) async {
+  final response = await fetchBasicData(
+      '${Api.DOWNLOADER_TORRENT_DETAIL}$downloaderId',
+      queryParameters: {'torrent_hash': torrentHash});
+  return response;
+}
+
+//*///@title 获取下载器分类信息
+///@description
+///@updateTime
 Future<CommonResponse> getDownloaderCategories(int downloaderId) async {
-  final response = await DioUtil().get(Api.DOWNLOADER_CATEGORIES,
-      queryParameters: {"downloader_id": downloaderId});
-  if (response.statusCode == 200) {
-    Logger.instance.w(response.data['data']);
-    final dataList = (response.data['data'] as List)
-        .map<DownloaderCategory>((item) => DownloaderCategory.fromJson(item))
-        .toList();
-    Logger.instance.w(dataList);
-    String msg = '共有${dataList.length}个分类';
-    return CommonResponse(data: dataList, code: 0, msg: msg);
-  } else {
-    String msg = '获取主页状态失败: ${response.statusCode}';
-    // GFToast.showToast(msg, context);
-    return CommonResponse(data: null, code: -1, msg: msg);
-  }
+  final response =
+      await fetchBasicList('${Api.DOWNLOADER_CATEGORY}$downloaderId');
+  return response;
+}
+
+//*///@title 创建下载器分类
+///@description
+///@updateTime
+Future<CommonResponse> createDownloaderCategory(
+    int downloaderId, Category category) async {
+  final response = await addData(
+      '${Api.DOWNLOADER_CATEGORY}$downloaderId', category.toJson());
+  return response;
+}
+
+//*///@title 修改下载器分类
+///@description
+///@updateTime
+Future<CommonResponse> editDownloaderCategory(
+    int downloaderId, Category category) async {
+  final response = await editData(
+      '${Api.DOWNLOADER_CATEGORY}$downloaderId', category.toJson());
+  return response;
+}
+
+//*///@title 删除下载器分类
+///@description
+///@updateTime
+Future<CommonResponse> deleteDownloaderCategory(
+    int downloaderId, String category) async {
+  final response = await removeData('${Api.DOWNLOADER_CATEGORY}$downloaderId',
+      queryParameters: {'category': category});
+  return response;
+}
+
+//*///@title 设置种子分类
+///@description
+///@updateTime
+Future<CommonResponse> addTorrentCategory(
+    int downloaderId, String tag, String torrentHashes) async {
+  final response = await editData('${Api.DOWNLOADER_SET_CATEGORY}$downloaderId',
+      {'tag': tag, 'torrent_hashes': torrentHashes});
+  return response;
+}
+
+//*///@title 删除种子分类
+///@description
+///@updateTime
+Future<CommonResponse> removeTorrentCategory(
+    int downloaderId, String tag, dynamic torrentHashes) async {
+  final response = await removeData(
+      '${Api.DOWNLOADER_SET_CATEGORY}$downloaderId',
+      queryParameters: {'tag': tag, 'torrent_hashes': torrentHashes});
+  return response;
+}
+
+//*///@title 获取下载器标签信息
+///@description
+///@updateTime
+Future<CommonResponse> getDownloaderTags(int downloaderId) async {
+  final response = await fetchBasicData('${Api.DOWNLOADER_TAGS}$downloaderId');
+  return response;
+}
+
+//*///@title 创建下载器标签
+///@description
+///@updateTime
+Future<CommonResponse> createDownloaderTags(
+    int downloaderId, String tag) async {
+  final response =
+      await addData('${Api.DOWNLOADER_TAGS}$downloaderId', {'tag': tag});
+  return response;
+}
+
+//*///@title 删除下载器标签
+///@description
+///@updateTime
+Future<CommonResponse> deleteDownloaderTags(
+    int downloaderId, String tag) async {
+  final response = await removeData('${Api.DOWNLOADER_TAGS}$downloaderId',
+      queryParameters: {'tag': tag});
+  return response;
+}
+
+//*///@title 设置种子标签
+///@description
+///@updateTime
+Future<CommonResponse> addTorrentTags(
+    int downloaderId, String tag, String torrentHashes) async {
+  final response = await editData('${Api.DOWNLOADER_SET_TAGS}$downloaderId',
+      {'tag': tag, 'torrent_hashes': torrentHashes});
+  return response;
+}
+
+//*///@title 删除种子标签
+///@description
+///@updateTime
+Future<CommonResponse> removeTorrentTags(
+    int downloaderId, String tag, dynamic torrentHashes) async {
+  final response = await removeData('${Api.DOWNLOADER_SET_TAGS}$downloaderId',
+      queryParameters: {'tag': tag, 'torrent_hashes': torrentHashes});
+  return response;
+}
+
+//*///@title 操作种子
+///@description
+///@updateTime
+Future<CommonResponse> controlTorrent(
+    {required int downloaderId, required Map<String, dynamic> command}) async {
+  final response =
+      await addData('${Api.DOWNLOADER_CONTROL}$downloaderId', command);
+  return response;
 }
 
 Future<CommonResponse> pushTorrentToDownloader({
@@ -92,6 +212,6 @@ Future<CommonResponse> pushTorrentToDownloader({
   } else {
     String msg = '获取主页状态失败: ${response.statusCode}';
     // GFToast.showToast(msg, context);
-    return CommonResponse(data: null, code: -1, msg: msg);
+    return CommonResponse.error(msg: msg);
   }
 }
