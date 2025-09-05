0aa9a9fdbf1d8e1604be4f4bc9929b56056f3980
ngfchl <ngfchl@126.comn>
2025-09-05 17:43:58 +0800

    add. 新增资源管理 API 访问方法

---
+++ lib/api/source.dart
@@ -0,0 +1,30 @@
+import '../models/common_response.dart';
+import 'api.dart';
+import 'hooks.dart';
+
+/// 获取
+Future<CommonResponse> getSourceListApi({String path = '/downloads'}) async {
+  return await fetchBasicData(Api.SOURCE_LIST, queryParameters: {"path": path});
+}
+
+/// 获取
+Future<CommonResponse> getSourceUrlApi({required String path}) async {
+  return await fetchData(Api.SOURCE_URL, queryParameters: {"file_path": path});
+}
+// ///  修改用户信息
+// editUserModelApi(UserModel user) async {
+//   String apiUrl = '${Api.AUTH_USER}/${user.id}';
+//   return await editData(apiUrl, user.toJson());
+// }
+//
+// /// 保存用户信息
+// addUserModelApi(UserModel user) async {
+//   String apiUrl = Api.AUTH_USER;
+//   return await addData(apiUrl, user.toJson());
+// }
+//
+// ///  修改用户信息
+// removeUserModelApi(UserModel user) async {
+//   String apiUrl = '${Api.AUTH_USER}/${user.id}';
+//   return await removeData(apiUrl);
+// }
