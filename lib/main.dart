88a865ea0da7464f9965b9f9ae37230fb2065612
ngfchl <ngfchl@126.com>
2025-11-14 09:24:12 +0800

    update. 优化main方法依赖加载

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:harvest/utils/logger_helper.dart';
 import 'package:harvest/utils/storage.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:window_manager/window_manager.dart';
 
 import 'app/routes/app_pages.dart';
@@ -16,71 +15,18 @@ import 'theme/theme_controller.dart';
 void main() async {
   // 初始化插件前需要在runApp之前调用初始化代码
   WidgetsFlutterBinding.ensureInitialized();
-
-  /// 必须调用这行！
-  MediaKit.ensureInitialized();
-  // 初始化 持久化数据信息
-  await SPUtil.getInstance();
+  Logger.instance.i("============初始化项目依赖===========");
   await initDependencies();
-  Get.testMode = false;
-  Get.put(ThemeController()); // 注入主题控制器
-  // 注册 HomeController 控制器
-  String? server = SPUtil.getLocalStorage('server');
-  if (server == null || server.length <= 10) {
-    SPUtil.setBool('isLogin', false);
-  } else {
-    await DioUtil().initialize(server);
-  }
-  // 强制竖屏
-  // SystemChrome.setPreferredOrientations(
-  //     [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
-  // 固定写法，处理状态栏背景颜色
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-  ));
-  // 必须加上这一行。
-  if (GetPlatform.isDesktop && !GetPlatform.isWeb) {
-    await windowManager.ensureInitialized();
-    double height =
-        SPUtil.getDouble('ScreenSizeHeight', defaultValue: 900).toDouble();
-    double width =
-        SPUtil.getDouble('ScreenSizeWidth', defaultValue: 1200).toDouble();
-    // Logger.instance.d('window size: $width, $height');
-    WindowOptions windowOptions = WindowOptions(
-      // size: Size(1200, 900),
-      size: Size(width, height),
-      center: true,
-      backgroundColor: Colors.transparent,
-      skipTaskbar: false,
-      titleBarStyle: TitleBarStyle.normal,
-      windowButtonVisibility: true,
-    );
-    windowManager.waitUntilReadyToShow(windowOptions, () async {
-      await windowManager.show();
-      await windowManager.focus();
-    });
-  }
-  // await Future.delayed(Duration(milliseconds: 3000), () {
-  // FlutterNativeSplash.remove();
-  // });
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  Logger.instance.i("============项目依赖初始化完成===========");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  Future<void> onInit(BuildContext context) async {
-    //延迟3秒
-    await Future.delayed(const Duration(seconds: 3));
-    await Future.delayed(const Duration(seconds: 1));
-    FlutterNativeSplash.remove();
-  }
-
   @override
   Widget build(BuildContext context) {
-    final themeController = Get.find<ThemeController>();
+    final themeController = Get.put<ThemeController>(ThemeController());
     Logger.instance.d("暗黑模式：${themeController.isDark.value}");
     Logger.instance.d("暗黑模式：${Get.isDarkMode}");
     Logger.instance.d("暗黑模式：${SPUtil.getBool('isDark')}");
@@ -105,8 +51,7 @@ class MyApp extends StatelessWidget {
 
                 // 如果出现异常值，使用默认值替代
                 if (safeTop > 80 || safeTop < 0) {
-                  print(
-                      'Detected abnormal top padding: $safeTop, using fallback.');
+                  print('Detected abnormal top padding: $safeTop, using fallback.');
                   safeTop = 24.0; // 合理默认值
                 }
 
@@ -119,8 +64,9 @@ class MyApp extends StatelessWidget {
               },
               locale: const Locale('zh', 'CN'),
               fallbackLocale: const Locale('en', 'US'),
-              onInit: () async {
-                await onInit(context);
+              onReady: () async {
+                await Future.delayed(const Duration(seconds: 3));
+                FlutterNativeSplash.remove();
               },
             );
           });
@@ -129,6 +75,61 @@ class MyApp extends StatelessWidget {
 }
 
 Future<void> initDependencies() async {
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-  Get.put<SharedPreferences>(prefs);
+  /// 必须调用这行！
+
+  Logger.instance.i("============初始化MediaKit===========");
+  MediaKit.ensureInitialized();
+  Logger.instance.i("============MediaKit初始化完成===========");
+  // 初始化 持久化数据信息
+  Logger.instance.i("============初始化SharedPreferences===========");
+  await SPUtil.getInstance();
+  Logger.instance.i("============SharedPreferences初始化完成===========");
+
+  Get.testMode = false;
+
+  Logger.instance.i("============初始化DioUtil===========");
+  String? server = SPUtil.getLocalStorage('server');
+  if (server == null || server.length <= 10) {
+    SPUtil.setBool('isLogin', false);
+  } else {
+    await DioUtil().initialize(server);
+  }
+  Logger.instance.i("============DioUtil初始化完成===========");
+  // 强制竖屏
+  // SystemChrome.setPreferredOrientations(
+  //     [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  // 固定写法，处理状态栏背景颜色透明问题
+  Logger.instance.i("============处理状态栏背景颜色透明问题===========");
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  Logger.instance.i("============处理状态栏背景颜色透明问题完成===========");
+  // 必须加上这一行。
+  Logger.instance.i("============初始化窗口管理器===========");
+  if (GetPlatform.isDesktop && !GetPlatform.isWeb) {
+    await windowManager.ensureInitialized();
+    double height = SPUtil.getDouble('ScreenSizeHeight', defaultValue: 900).toDouble();
+    double width = SPUtil.getDouble('ScreenSizeWidth', defaultValue: 1200).toDouble();
+    Logger.instance.d('已缓存的窗口大小: $width, $height');
+    WindowOptions windowOptions = WindowOptions(
+      size: Size(width, height),
+      center: true,
+      backgroundColor: Colors.transparent,
+      skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.normal,
+      windowButtonVisibility: true,
+    );
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
+  Logger.instance.i("============窗口管理器初始化完成===========");
+  // await Future.delayed(Duration(milliseconds: 3000), () {
+  // FlutterNativeSplash.remove();
+  // });
+  Logger.instance.i("============设置SystemUiMode为edgeToEdge===========");
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  Logger.instance.i("============设置SystemUiMode为edgeToEdge完成===========");
 }
