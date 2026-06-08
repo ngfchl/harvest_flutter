003ce2c5a2be428aec5a7243fed120d628bf67d7
ngfchl <ngfchl@users.noreply.github.com>
2026-06-08 15:59:50 +0000

    deploy: b3627d14a0ccf1306f3a676d29383d8eb61b71a3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1146017735"
+    serviceWorkerVersion: "189253158"
   }
 });
