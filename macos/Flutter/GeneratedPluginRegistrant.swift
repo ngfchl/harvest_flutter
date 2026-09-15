2fffdc24d6859540e4cb0c223cda4dc21beb2194
ngfchl <ngfchl@126.com>
2026-09-15 15:33:34 +0800

    chore: 更新依赖版本和构建配置

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
-import file_picker
+import file_picker_darwin
 import file_selector_macos
 import flutter_inappwebview_macos
 import flutter_local_notifications
